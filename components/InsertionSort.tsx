dfdf9ebde4d4ce89a1a6c5d6396358cb35e10d01
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2019-11-12 00:18:06 +0900

    feat: insertion sort

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -3,12 +3,36 @@ import { useState } from 'react'
 
 const getArr = () => shuffle(range(1,11))
 
+const swap = (arr: number[], a: number, b: number) => {
+  const tmp = arr[a]
+  arr[a] = arr[b]
+  arr[b] = tmp
+}
+
+const sort = (arr: number[]) => {
+  // https://en.wikipedia.org/wiki/Insertion_sort
+  let i = 1
+  while (i < arr.length) {
+    let j = i
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      swap(arr, j, j - 1)
+      j = j - 1
+    }
+    i = i + 1
+  }
+}
+
 export default () => {
 
   const [arr, setArr] = useState(getArr())
 
   const handleShuffle = () => {
     setArr(getArr())
+  }
+  const handleSort = () => {
+    const sortedArr = [...arr]
+    sort(sortedArr)
+    setArr(sortedArr)
   }
 
   return (
@@ -21,7 +45,7 @@ export default () => {
 
       <div className='buttonBox'>
         <button onClick={handleShuffle}>shuffle</button>
-        <button>sort</button>
+        <button onClick={handleSort}>sort</button>
       </div>
 
       <style jsx>{`
